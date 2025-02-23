0e2a6340843cd20db745699da10666c00c845d2a
lidetu tesfaye <lidtmariamtesfu@gmail.com>
2025-02-23 05:41:21 -0800

    updating and  adding the FBV and CBV

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,11 +6,11 @@ from django.views.generic import ListView, DeleteView
 #Function Based View
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'books/list_books.html', {'books': books})
+    return render(request, 'templates/list_books.html', {'books': books})
 
 def list_authors(request):
     authors = Author.objects.all()
-    return render(request, 'authors/list_authors.html', {'authors': authors})
+    return render(request, 'templates/list_authors.html', {'authors': authors})
 
 # Class Based View
 class BookListView(ListView):
