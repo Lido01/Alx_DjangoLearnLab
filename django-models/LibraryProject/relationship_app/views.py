586d0d0870713bc8e7cfb6613afbfa062cf866aa
lidetu tesfaye <lidtmariamtesfu@gmail.com>
2025-02-23 06:25:58 -0800

    create user authentication login, register

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from .models import Author,Book, Librarian
 from .models import Library
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth import  authenticate,login, logout
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+#register View
+class Register(CreateView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+ 
+ 
+# Login view
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+
+
+
 
 #Function Based View
 def list_books(request):
@@ -24,3 +43,5 @@ class DetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library_detail"
+
+
