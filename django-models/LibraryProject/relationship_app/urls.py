42cef319a18b11ec6fef05ddf998310f614c1f31
lidetu tesfaye <lidtmariamtesfu@gmail.com>
2025-02-23 06:33:03 -0800

    create user authentication modify

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import list_authors, list_books, BookListView
+from .views import list_authors, list_books, BookListView , LibraryDetailView
+
+
 urlpatterns = [
 
     path('/home/book_list/',  list_books, name="book_list" ),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,7 @@ from django.views.generic.detail import DetailView
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView,LogoutView
-from django.contrib.auth import  authenticate,login, logout
+from django.contrib.auth import  login, logout, authenticate
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
@@ -16,7 +16,7 @@ class Register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
  
- 
+
 # Login view
 class CustomLoginView(LoginView):
     template_name = 'login.html'
@@ -39,7 +39,7 @@ class BookListView(ListView):
     template_name = "relationship_app/list_books.html"
     context_object_name = "book"
      
-class DetailView(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library_detail"
