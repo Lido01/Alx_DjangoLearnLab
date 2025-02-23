4c119c49e3214c26be2063b5e64fd7433fea9410
lidetu tesfaye <lidtmariamtesfu@gmail.com>
2025-02-23 06:00:55 -0800

    adding detail view in the CBV and render it

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,14 +2,11 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 
 Book.objects # The book object
-
 # Query all books by a specific author.
-author_name1 = Author.objects.create("Gadissa")
+author_name = Author.objects.create("Gadissa")
 author_name2 = Author.objects.create("Sinaf")
-author_name3 = Author.objects.create("lidex")
-
-Author.objects.get(name=author_name2)
-author = Author.objects.get(id=2)
+Author.objects.get(name=author_name)
+author = Author.objects.get(id=1)
 Book.objects.filter(author=author)
 
 
@@ -19,9 +16,8 @@ library_name2 = Library.objects.create(name="Financial")
 library_name3 = Library.objects.create(name="Business")
 
 
-li1 = Library.objects.get(name=library_name)
-li2 = Library.objects.get(name=library_name3)
-li2.books.all()
+library1 = Library.objects.get(name=library_name)
+library1.books.all()
 
 # Retrieve the librarian for a library.
-Librarian.objects.get(library=li2)
+Librarian.objects.get(library=library1)
