6583431f3bca83cbf590c4bcbf3ff2837a6db889
lidetu tesfaye <lidtmariamtesfu@gmail.com>
2025-02-24 05:11:14 -0800

    login and out page updating

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from .views import list_authors, BookListView , LibraryDetailView
 from .views import list_books
+from . import views
 
 
 urlpatterns = [
 
-    path('/home/book_list/',  list_books, name="book_list" ),
-    path('/home/list_author/',  list_authors, name=" list_author" ),
+    path('home/book_list/',  list_books, name="book_list" ),
+    path('home/list_author/',  list_authors, name=" list_author" ),
+    
 
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/',  views.Login.as_view(template_name='templates/login.html'), name='login'),
+    
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,39 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Author,Book, Librarian
 from .models import Library
+
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView,LogoutView
-from django.contrib.auth import  login, logout, authenticate
+from django.contrib.auth import logout, authenticate
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-
-#register View
-class Register(CreateView):
-    template_name = 'register.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
- 
-
-# Login view
-class CustomLoginView(LoginView):
-    template_name = 'login.html'
-
-class LogoutView(LogoutView):
-
-
+from django.contrib.auth.models import User
+from django.views import View
+from django.urls import reverse_lazy
 
 #Function Based View
-
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 def list_authors(request):
     authors = Author.objects.all()
-    return render(request, 'relationship/list_authors.html', {'authors': authors})
+    return render(request, "relationship_app/list_authors.html", {'authors': authors})
+    return render(request, "home/register.html", {"form": form})
 
 # Class Based View
 class BookListView(ListView):
@@ -45,5 +35,30 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library_detail"
+
+#Home page View
+#register View
+class RegisterView(View):
+    template_name = 'templates/register.html'
+    success_url = reverse_lazy("login")
+    
+    def get(self, request):
+        form =  UserCreationForm()
+        return render(request, 'templates/register.html', {'form': form})
+    
+    template_name = 'templates/register.html'
+    def  post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+                user =  form.save()
+                user.save()
+                login(request, user)
+                return redirect(self.success_url)
+        else:
+            form = UserCreationForm()
+        return render(request, self.template_name, {"form": form})
+# Login view
+class Login(LoginView):
+    template_name = 'templates/login.html'
 
 
