fa385d81c2b85f1ed7d40aa7e151e2809b40bd1a
lidetu tesfaye <lidtmariamtesfu@gmail.com>
2025-02-28 05:03:23 -0800

    updating role based control with templates of admin, librarian, member

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -145,16 +145,16 @@ def is_member(user):
     return user.is_authenticated and user.userprofile.role == 'Member'
 
 @user_passes_test(is_admin, login_url='login')
-def admin_view(request):
-    return render(request, 'admin_view.html')
+def Admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(is_librarian, login_url='login')
-def librarian_view(request):
-    return render(request, 'librarian_view.html')
+def Librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
 
 @user_passes_test(is_member, login_url='login')
-def member_view(request):
-    return render(request, 'member_view.html')
+def Member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 
 
