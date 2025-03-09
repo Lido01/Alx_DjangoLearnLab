ab72c592bfefb3ed5cfcf6f546aa94665569f1d8
lidetu tesfaye <lidtmariamtesfu@gmail.com>
2025-03-09 06:47:16 -0700

    Authentication by  token

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path, include
-from .views import BookList, BookViewSet
+from .views import BookList,BookCreate, BookViewSet
 #from django.views import  
 from rest_framework.routers import DefaultRouter
-
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
-router.register(r'books_all', BookViewSet, basename='book_all')
+router.register(r'books_all', BookViewSet, basename='books_all')
 
 urlpatterns = [
 
     path("books/", BookList.as_view(), name="book-list"),
-    path('', include(router.urls)), #THis includes all routes registered with the router
+    path("books_create/", BookCreate.as_view(), name="book-create"),
+    path("obtain_token/", obtain_auth_token, name="obtain_token"),
+    path('viewset/', include(router.urls)), #THis includes all routes registered with the router
 
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,14 +3,31 @@ from .serializers import BookSerializer
 from .models import Book
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+
 
 
 class BookList(generics.ListAPIView):
+    authentication_classes = [BasicAuthentication, SessionAuthentication] # allow user just by
+    permission_classes = [IsAuthenticated]
     serializer_class = BookSerializer
-    queryset = Book
+    queryset = Book.objects.all()
+
+class BookCreate(generics.CreateAPIView):
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
+    authentication_classes = [TokenAuthentication] #[BasicAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticated] #Permitts if user loged in. if not it restrics access from
+
