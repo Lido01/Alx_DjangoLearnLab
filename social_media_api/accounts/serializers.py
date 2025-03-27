9ff9bfd3cc622f78d3a4bb72b387b65602f1bef6
lidetu tesfaye <lidtmariamtesfu@gmail.com>
2025-03-27 06:47:26 -0700

    Creating social_media_api by creating CustomUser

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,12 @@
+from .models import CustomUser
+from rest_framework import serializers
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+
+        model = CustomUser
+        fields = ["username", "email", "bio"]
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(username=validated_data["username"], email=validated_data["email"], password=validated_data["password"])
+        return user
