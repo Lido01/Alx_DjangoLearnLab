99afb20dcf23918b385a34fa4dbef46a8cf23cc1
lidetu tesfaye <lidtmariamtesfu@gmail.com>
2025-03-28 03:26:53 -0700

    creating the register and login auth

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,12 +1,21 @@
 from .models import CustomUser
 from rest_framework import serializers
 
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["username", "email"]
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
-
         model = CustomUser
-        fields = ["username", "email", "bio"]
+        fields = ["id", "username", "email", "password", "bio"]
     def create(self, validated_data):
         user = CustomUser.objects.create_user(username=validated_data["username"], email=validated_data["email"], password=validated_data["password"])
         return user
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import CustomUserSerializer, RegisterSerializer, LoginSerializer
+from .models import CustomUser
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+    # def get_queryset(self):
+    #     qs = CustomUser.
+    #     return super().get_queryset()
+class LoginView(generics.CreateAPIView):
+    serializer_class = LoginSerializer
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("username")
+        password = request.data.get("passsword")
+        user = authenticate(username=username, password=password)
+        if username is not None:
+            refresh = RefreshToken.for_user(user)
+
+            custom_user_serializer = CustomUserSerializer(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'user': custom_user_serializer.data
+            }, 200)
+        else:
+            return Response({"detail": "Invalid Credentials"}, status=401)
